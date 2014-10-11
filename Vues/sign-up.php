41e9b08ad58206cad4df67239f355d72653088a7
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-10-11 18:20:57 +0200

    Fix sign-up

--- Vues/sign-up.php
+++ Vues/sign-up.php
@@ -10,41 +10,39 @@ require_once("header.php");
 	<form role="form">
 
 		<div class="form-group">
-			<label for="mail">Nom</label>
+			<label for="nom">Nom</label>
 			<div class="input-group">
 			  <div class="input-group-addon">@</div>
-			  <input class="form-control" type="text" placeholder="SAM">
+			  <input name="nom" class="form-control" type="text" placeholder="SAM">
 			</div>
 		</div>
 
 		<div class="form-group">
-		<label for="mail">Adresse email</label>
-		<div class="input-group">
-		  <div class="input-group-addon">@</div>
-		  <input class="form-control" type="email" placeholder="sam@hotmail.com">
-		</div>
+			<label for="mail">Adresse email</label>
+			<div class="input-group">
+		 		<div class="input-group-addon">@</div>
+		  		<input name="email" class="form-control" type="email" placeholder="sam@hotmail.com">
+			</div>
 		</div>
 
 		<div class="form-group">
-		<label for="password">Mot de passe</label>
-
+			<label for="password">Mot de passe</label>
 			<div class="input-group">
-		  <div class="input-group-addon">
-		  	<span class="glyphicon glyphicon-lock"></span>
-		  </div>
-		  <input type="password" class="form-control" id="password" placeholder="*********">
-		</div>
+		  		<div class="input-group-addon">
+		  			<span class="glyphicon glyphicon-lock"></span>
+		  		</div>
+		  		<input name="password" type="password" class="form-control" id="password" placeholder="*********">
+			</div>
 		</div>
 
 		<div class="form-group">
-		<label for="password">Confirmez le mot de passe</label>
-
+			<label for="password">Confirmez le mot de passe</label>
 			<div class="input-group">
-		  <div class="input-group-addon">
-		  	<span class="glyphicon glyphicon-lock"></span>
-		  </div>
-		  <input type="password" class="form-control" id="password" placeholder="*********">
-		</div>
+		  		<div class="input-group-addon">
+		  			<span class="glyphicon glyphicon-lock"></span>
+		 		 </div>
+		  		<input type="password" class="form-control" id="password" placeholder="*********">
+			</div>
 		</div>
 
 	  <div class="form-group col-lg-6 col-lg-offset-3">
