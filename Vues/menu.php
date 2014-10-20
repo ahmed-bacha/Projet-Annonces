17dcabd8a4590abcb5e929e44aa3f76c8a2c16a4
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-10-20 12:28:13 +0200

    Vues dropdown

--- Vues/menu.php
+++ Vues/menu.php
@@ -17,7 +17,10 @@
         <ul class="nav navbar-nav">
 
             <li class="active">
-                <a href="#">Home</a>
+                <a href="#">
+                    <span class="glyphicon glyphicon-home">
+                    </span>                    
+                </a>
             </li>
         </ul>
 
@@ -26,33 +29,53 @@
                 <input type="text" placeholder="Search" class="form-control">
             </div>
         </form>
-        <ul class="nav navbar-nav navbar-right">
-            <li>
-                <a href="single-annonce.php">Annonce</a>
-            </li>
-            
-            <?php 
-            if (isset($userM)) {
-                ?>
-                <li>
-                    <?php echo '<a href="profile.php?id='.$userM->id.'">' ."Bonjour : ".$userM->nom.'</a>'; ?>
-                </li>
-                <li>
-                    <a href="log-out.php">Logout</a>
-                </li>
-            <?php
 
-            }else{
-                ?>
-                <li>
-                    <a href="sign-up.php">Sign Up</a>
-                </li>
-                <li>
-                    <a href="log-in.php">Login</a>
-                </li>
-            <?php
-            }
-             ?>
+        <ul class="nav navbar-nav navbar-right">
+
+
+        <!-- BLOC DES VUES  -->
+
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                Vues 
+                <span class="caret"></span>
+            </a>
+          <ul class="dropdown-menu" role="menu">
+            <li>
+                <a href="single-annonce.php">Annonces</a>
+            </li>
+          </ul>
+        </li>
+
+        <!-- Activation de la fonction DropDown du menu -->
+        <script type="text/javascript">
+            $('.dropdown-toggle').dropdown();
+        </script>
+
+        <!-- END BLOC DES VUES  -->
+            
+        <?php 
+        if (isset($userM)) {
+            ?>
+            <li>
+                <?php echo '<a href="profile.php?id='.$userM->id.'">' ."Bonjour : ".$userM->nom.'</a>'; ?>
+            </li>
+            <li>
+                <a href="log-out.php">Logout</a>
+            </li>
+        <?php
+
+        }else{
+            ?>
+            <li>
+                <a href="sign-up.php">Sign Up</a>
+            </li>
+            <li>
+                <a href="log-in.php">Login</a>
+            </li>
+        <?php
+        }
+         ?>
         </ul>
     </div>
 </nav>
