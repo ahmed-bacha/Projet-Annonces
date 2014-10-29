5a1172366213ba28eb1aa53931e7f81835b8ca7a
sihamTSE <samentague@hotmail.com>
2014-10-29 09:58:43 +0100

    MAJ vue réponse-annonce

--- Vues/reponse-annonce.php
+++ Vues/reponse-annonce.php
@@ -6,7 +6,12 @@ require_once("header.php");
 
 <!-- Page Content -->
 <div class="row">
-	<div class="col-md-12" style='border: 1px solid black'>
+	<div class="col-xs-12 col-sm-12 col-lg-offset-2 col-md-12 col-lg-offset-2 col-lg-8">
+		    <blockquote>
+  				<h4> Répondre à l'annonce</h4>
+		   	</blockquote>
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">		   	
 		<form role="form">
 		  <div class="form-group">
 		    <label for="exampleInputNom">Nom</label>
@@ -15,7 +20,10 @@ require_once("header.php");
 			  <input type="text" class="form-control" placeholder="Votre nom">
 			</div>
 		  </div>
-		  
+		</div>
+	</div>
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">				  
 		  <div class="form-group">
 		    <label for="exampleInputTeleph">Téléphone</label>
 		    <div class="input-group">
@@ -23,6 +31,10 @@ require_once("header.php");
 			  <input type="text" class="form-control" placeholder="Votre numéro de téléphone">
 			</div>
 		  </div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">			  
 		 <div class="form-group">
 		    <label for="exampleInputEmail">Email</label>
 		    <div class="input-group">
@@ -30,10 +42,18 @@ require_once("header.php");
 			  <input type="text" class="form-control" placeholder="Votre email">
 			</div>
 		  </div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">			  
 		  <div class="form-group">
 		  	<label for="exampleInputText">Texte</label>
 		  	<textarea class="form-control" placeholder="Votre texte" rows="2"></textarea>
 		  </div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">			  
           <div class="form-group">
           	<div class="checkbox">
               <label> <input type="checkbox"> Recevoir une copie de cet email </label>
