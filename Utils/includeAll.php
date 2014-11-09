66ba19ea524f6492d0b9b2858d5cbe5a9e3e1484
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-11-09 14:37:35 +0100

    Ajout lib MailJet

--- Utils/includeAll.php
+++ Utils/includeAll.php
@@ -7,15 +7,24 @@ define("LOCAL_PATH", $dir."/");
 //echo LOCAL_PATH;
 
 
-/* include MODELES*/
+/* 
+	include MODELES
+*/
 require_once(LOCAL_PATH."Modeles/spdo.php");
 require_once(LOCAL_PATH."Modeles/ModelInterface.php");
 
 require_once(LOCAL_PATH."Modeles/UtilisateurM.php");
 
-/* include CONTROLLEURS*/
+/* 
+	include CONTROLLEURS
+*/
 require_once(LOCAL_PATH."Controlleurs/UtilisateurC.php");
 
+
+/* 
+	include lib tiers 
+*/
+require_once(LOCAL_PATH."Utils/php-mailjet.class-mailjet-0.1-2.php");
 
 
 ?>
