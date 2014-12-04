3d3cb815d69f8512f03c125642a7919065477264
olivier marin <oliviermarin92@gmail.com>
2014-12-04 11:57:11 +0100

    remarche add annonce.js
    salut

--- Vues/js/add-annonce.js
+++ Vues/js/add-annonce.js
@@ -1,48 +1,61 @@
-      $( document ).ready(function() {
+// JQuery start ------------------------------------------------
 
-      $( "#erreur" ).hide();
-      $( "#success" ).hide();
+$( document ).ready(function() {
 
-      $("form#form").submit(function(event){
+  $( "#erreur" ).hide();
+  $( "#success" ).hide();
 
-        //disable the default form submission
-        event.preventDefault();
+  // fonction start---------------------------------------------
+  $("form#form").submit(function(event){
 
-        var formData    = new FormData($(this)[0]);
-        // console.log(formData.toString());
+    //disable the default form submission
+    event.preventDefault();
 
-        $.ajax({
-          url: 'traitement-image.php',
-          type: 'POST',
-          data: formData,
-          mimeType:"multipart/form-data",
-          contentType: false,
-          async: false,
-          cache: false,
-          processData: false,
+    var formData    = new FormData($(this)[0]);
+    console.log(formData.toString());
 
-          success: function(result)
-          {
-            if (jQuery.isEmptyObject(result) == false) {
-              $( "#erreur" ).show();
-              ( "#success" ).hide();
-              $('#erreur').html(result);
-            }else{
-              $( "#erreur" ).hide();
-              $( "#success" ).show();
-              $( "#success" ).html("Annonce créée !!!");
-              $("form#form").slideUp("slow");
-            }
+    // ajax request---------------------------------------------
 
-            console.log(result);
-          }
+    $.ajax({
+      url: 'traitement-image.php',
+      type: 'POST',
+      data: formData,
+      mimeType:"multipart/form-data",
+      contentType: false,
+      async: false,
+      cache: false,
+      processData: false,
 
-        });
+      success: function(result){
 
+        if (jQuery.isEmptyObject(result) == false) {
 
-        return false;
-      });
+          $( "#erreur" ).show();
+          ( "#success" ).hide();
+          $('#erreur').html(result);
 
+        } else {
+
+          $( "#erreur" ).hide();
+          $( "#success" ).show();
+          $( "#success" ).html("Annonce créée !!!");
+          $("form#form").slideUp("slow");
+
+        }
+
+        console.log(result);
+
+    }
+
+  });
+
+  // end of ajax request--------------------------------------
+
+  return false;
+
+  });
+
+  // fonction end --------------------------------------------
 
       $("#file1").change(function() {
 
@@ -66,84 +79,115 @@
             }
           });
 
-          function imageIsLoaded1(e) {
+  // fonction start ------------------------------------------
 
-            $('#img1').attr('src', e.target.result);
+  function imageIsLoaded1(e) {
 
-          };
+    $('#img1').attr('src', e.target.result);
 
-          $("#file2").change(function() {
+  }
 
-
-            var file = this.files[0];
-            console.log(file);
-            var imagefile = file.type;
-            //alert(imagefile);
-            var match= ["image/jpeg","image/png","image/jpg"];
-            if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2])))
-              {
-                return false;
-              }
-              else
-                {
-                  var reader = new FileReader();
-                  reader.onload = imageIsLoaded2;
-
-                  reader.readAsDataURL(this.files[0]);
-
-                }
-              });
-
-              function imageIsLoaded2(e) {
-
-                $('#img2').attr('src', e.target.result);
-
-              };
-
-              $("#file3").change(function() {
+  $("#file2").change(function() {
 
 
-                var file = this.files[0];
-                console.log(file);
-                var imagefile = file.type;
-                //alert(imagefile);
-                var match= ["image/jpeg","image/png","image/jpg"];
-                if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2])))
-                  {
-                    return false;
-                  }
-                  else
-                    {
-                      var reader = new FileReader();
-                      reader.onload = imageIsLoaded3;
+    var file = this.files[0];
+    console.log(file);
+    var imagefile = file.type;
+    //alert(imagefile);
+    var match= ["image/jpeg","image/png","image/jpg"];
 
-                      reader.readAsDataURL(this.files[0]);
+    if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2]))){
+        return false;
+    } else {
 
-                    }
-                  });
+        var reader = new FileReader();
 
-                  function imageIsLoaded3(e) {
+        reader.onload = imageIsLoaded2;
 
-                    $('#img3').attr('src', e.target.result);
-                  };
+        reader.readAsDataURL(this.files[0]);
 
+    }
 
+  });
 
-                  $("#delete-icon-1").click(function() {
-                    $('#file1').val('');
-                    $('#img1').attr('src',"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgdmlld0JveD0iMCAwIDMwMCAzMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjExMy4wMTU2MjUiIHk9IjE1MCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxOXB4O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjMwMHgzMDA8L3RleHQ+PC9nPjwvc3ZnPg==");
+  // fonction end --------------------------------------------
 
-                  });
+  // fonction start ------------------------------------------
 
-                  $("#delete-icon-2").click(function() {
-                    $('#file2').val('');
-                    $('#img2').attr('src',"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgdmlld0JveD0iMCAwIDMwMCAzMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjExMy4wMTU2MjUiIHk9IjE1MCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxOXB4O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjMwMHgzMDA8L3RleHQ+PC9nPjwvc3ZnPg==");
+  function imageIsLoaded2(e) {
 
-                  });
+      $('#img2').attr('src', e.target.result);
 
-                  $("#delete-icon-3").click(function() {
-                    $('#file3').val('');
-                    $('#img3').attr('src',"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgdmlld0JveD0iMCAwIDMwMCAzMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjExMy4wMTU2MjUiIHk9IjE1MCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxOXB4O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjMwMHgzMDA8L3RleHQ+PC9nPjwvc3ZnPg==");
+  };
 
-                  });
-                });
+  // fonction end --------------------------------------------
+
+  // fonction start ------------------------------------------
+
+  $("#file3").change(function() {
+
+    var file = this.files[0];
+    console.log(file);
+    var imagefile = file.type;
+    //alert(imagefile);
+    var match= ["image/jpeg","image/png","image/jpg"];
+
+    if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2]))){
+
+        return false;
+
+    } else {
+
+        var reader = new FileReader();
+
+        reader.onload = imageIsLoaded3;
+
+        reader.readAsDataURL(this.files[0]);
+
+    }
+
+  });
+
+  // fonction end --------------------------------------------
+
+  // fonction start ------------------------------------------
+
+  function imageIsLoaded3(e) {
+
+    $('#img3').attr('src', e.target.result);
+
+  };
+
+  // fonction end --------------------------------------------
+
+  // fonction end --------------------------------------------
+
+  $("#delete-icon-1").click(function() {
+
+    $('#file1').val('');
+
+    $('#img1').attr('src',"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgdmlld0JveD0iMCAwIDMwMCAzMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjExMy4wMTU2MjUiIHk9IjE1MCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxOXB4O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjMwMHgzMDA8L3RleHQ+PC9nPjwvc3ZnPg==");
+
+  });
+
+  $("#delete-icon-2").click(function() {
+
+    $('#file2').val('');
+
+    $('#img2').attr('src',"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgdmlld0JveD0iMCAwIDMwMCAzMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjExMy4wMTU2MjUiIHk9IjE1MCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxOXB4O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjMwMHgzMDA8L3RleHQ+PC9nPjwvc3ZnPg==");
+
+  });
+
+  $("#delete-icon-3").click(function() {
+
+    $('#file3').val('');
+
+    $('#img3').attr('src',"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgdmlld0JveD0iMCAwIDMwMCAzMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMzAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjExMy4wMTU2MjUiIHk9IjE1MCIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxOXB4O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjMwMHgzMDA8L3RleHQ+PC9nPjwvc3ZnPg==");
+
+  });
+
+  // fonction end --------------------------------------------
+
+});
+
+// JQuery end --------------------------------------------
