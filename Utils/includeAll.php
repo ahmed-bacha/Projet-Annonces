85701694cb79bc79a5562463e01ac1655f542138
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-12-08 11:53:23 +0100

    ADD fonction show

--- Utils/includeAll.php
+++ Utils/includeAll.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $dir = dirname(__DIR__);
 
@@ -7,7 +7,7 @@ define("LOCAL_PATH", $dir."/");
 //echo LOCAL_PATH;
 
 
-/* 
+/*
 	Constantes STATUS Annonce
 */
 define("NON_TRAITE", 10);
@@ -15,7 +15,7 @@ define("TRAITE", 20);
 define("A_SUPPRIMER", 30);
 
 
-/* 
+/*
 	include MODELES
 */
 require_once(LOCAL_PATH."Modeles/spdo.php");
@@ -24,17 +24,23 @@ require_once(LOCAL_PATH."Modeles/ModelInterface.php");
 require_once(LOCAL_PATH."Modeles/UtilisateurM.php");
 require_once(LOCAL_PATH."Modeles/AnnonceM.php");
 
-/* 
+/*
 	include CONTROLLEURS
 */
 require_once(LOCAL_PATH."Controlleurs/UtilisateurC.php");
 require_once(LOCAL_PATH."Controlleurs/AnnonceC.php");
 
 
-/* 
-	include lib tiers 
+/*
+	include lib tiers
 */
 require_once(LOCAL_PATH."Utils/php-mailjet.class-mailjet-0.1-2.php");
+
+
+/*
+fonctions utils
+*/
+require_once(LOCAL_PATH."Utils/utilities.php");
 
 
 ?>
---
+++ Utils/utilities.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+* Affichage propore des tableaux PHP
+* @param  ARRAY : tableau a afficher
+*/
+function show($_array){
+
+    echo '<pre>';
+    print_r($_array);
+    echo '</pre>';
+
+}
+
+
+ ?>
