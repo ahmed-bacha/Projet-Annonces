af6b9ede32161b394fdf4e891ade6a74335c30b5
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-12-08 12:03:38 +0100

    ADD fonction md5ForFile

--- Utils/utilities.php
+++ Utils/utilities.php
@@ -13,5 +13,23 @@ function show($_array){
 
 }
 
+/**
+* Calcul la signature MD5 d'un fichier
+* @param  STRING file_path : chemin vers le fichier
+* @param  STRING signature : signature MD5 du fichier
+*/
+function md5ForFile($file_path){
+
+  if(file_exists($file_path){
+
+    return md5_file($file_path);
+
+  }else{
+
+    return "invalideFilePath";
+  }
+
+}
+
 
  ?>
