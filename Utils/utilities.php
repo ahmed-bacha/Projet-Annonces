676e964683d6c9fbdcf4bca7ee0860f8f6c35b20
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-12-08 12:13:16 +0100

    FIX #1 utilities

--- Utils/utilities.php
+++ Utils/utilities.php
@@ -15,12 +15,12 @@ function show($_array){
 
 /**
 * Calcul la signature MD5 d'un fichier
-* @param  STRING file_path : chemin vers le fichier
+* @param  STRING file_path : chemin vers le fichier ABSOLUT
 * @param  STRING signature : signature MD5 du fichier
 */
 function md5ForFile($file_path){
 
-  if(file_exists($file_path){
+  if(file_exists($file_path)){
 
     return md5_file($file_path);
 
