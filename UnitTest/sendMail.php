f816483e56931f1be5e338f6fb64f21216c58cbf
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-12-10 15:55:20 +0100

    ADD fonction sendEmail

--- UnitTest/sendMail.php
+++ UnitTest/sendMail.php
@@ -1,11 +1,12 @@
 <?php
 
 /*
-	Sending email via MailJet API
+	Sending email via MailGUN API
 */
 
 require_once("../Utils/includeAll.php");
 
+/*
 use Mailgun\Mailgun;
 
 # Instantiate the client.
@@ -15,16 +16,29 @@ $domain = "https://api.mailgun.net/v2/sandbox360b4037a0f140c6be0f136ce5c80ea7.ma
 
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
-  'from'    => 'Projet-Annonces MailGun <postmaster@sandbox360b4037a0f140c6be0f136ce5c80ea7.mailgun.org>',
-  'to'      => 'kimkero13@gmail.com',
-  'subject' => 'Hello',
-  'text'    => 'Testing some Mailgun awesomness!',
-  'html'    => '<html>Hello</html>'));
+  'from'        => 'Projet-Annonces MailGun <postmaster@sandbox360b4037a0f140c6be0f136ce5c80ea7.mailgun.org>',
+  'to'          => 'kimkero13@gmail.com',
+  'h:Reply-To'  => 'ahmedbacha69@gmail.com',
+  'subject'     => 'Hello',
+  'html'        => 'hello de TSE, hello <b>Team4J</b>'));
 
 
 var_dump($result);
 
 echo $result->http_response_code;
+*/
+
+
+/* TEST de la fonction sendEmail*/
+
+$_array = array(
+    'to'          => 'kimkero13@gmail.com',
+    'reply-to'    => 'ahmedbacha69@gmail.com',
+    'sujet'       => 'Hello',
+    'message'     => 'Hi from TSE, hello <b>Team4J</b>'
+);
+
+echo sendEmail($_array);
 
 
  ?>
--- Utils/utilities.php
+++ Utils/utilities.php
@@ -32,4 +32,40 @@ function md5ForFile($file_path){
 }
 
 
+/**
+* Envoi un mail
+* @param  array(
+*     'to'        => '',
+*     'sujet'     => 'Hello',
+*     'reply-to'  => 'address@domain.com',
+*     'message'   => 'Testing some Mailgun awesomness!');
+*
+*/
+
+function sendEmail($_array){
+
+
+  require_once("includeAll.php");
+  
+  $mgClient = new Mailgun\Mailgun('key-0b2cb3464af253c6c7317430cbe59abf');
+
+  $domain = "https://api.mailgun.net/v2/sandbox360b4037a0f140c6be0f136ce5c80ea7.mailgun.org";
+
+  $result = $mgClient->sendMessage($domain, array(
+    'from'        => 'Projet-Annonces MailGun <postmaster@sandbox360b4037a0f140c6be0f136ce5c80ea7.mailgun.org>',
+    'to'          => $_array['to'],
+    'h:Reply-To'  => $_array['reply-to'],
+    'subject'     => $_array['sujet'],
+    'html'        => $_array['message']
+  ));
+
+  if($result->http_response_code == 200){
+    return true;
+  }
+
+  return false;
+}
+
+
+
  ?>
