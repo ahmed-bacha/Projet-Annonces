02c9cf08248bd1223564bcbd7a88f013bda15f5d
nbouherrou <nacerbouherrou@gmail.com>
2014-12-14 12:24:32 +0100

    sign-up traitememt jQuery

--- Vues/js/sign-up.js
+++ Vues/js/sign-up.js
@@ -55,3 +55,241 @@ $( document ).ready(function() {
   });
 
 });
+
+
+$(function(){
+
+  var nom         = $('input[name="nom"]');
+
+  var email      = $('input[name="email"]');
+
+  var password1       = $('input[name="password"]');
+
+  var password2      = $('input[name="password_confirme"]');
+
+  var submit      = $('button[name="submit"]');
+
+
+  //--------------------------------
+  // treatment
+  //--------------------------------
+
+  nom.blur(function(){
+
+    treatField($(this));
+
+  });
+
+  nom.focus(function(){
+
+    setStandard($(this));
+
+  });
+
+  email.blur(function(){
+
+    treatEmailField($(this));
+  });
+
+  email.focus(function(){
+
+    setStandard($(this));
+
+  });
+
+  password1.blur(function(){
+
+    treatMdp1Field($(this))
+  });
+
+  password1.focus(function(){
+
+    setStandard($(this));
+
+  });
+
+  password2.blur(function(){
+
+    treatMdp2Field($(this))
+  });
+
+  password2.focus(function(){
+
+    setStandard($(this));
+
+  });
+
+  stateSubmitButton();
+
+
+  //--------------------------------
+  // function called during treatment
+  //--------------------------------
+
+  function setOk(element){
+
+    element.closest('div').addClass('has-success');
+
+    element.next().addClass('glyphicon-ok').show();
+
+    stateSubmitButton(submit);
+
+  }
+
+  function setKo(element){
+
+    element.closest('div').addClass('has-error');
+
+    element.next().addClass('glyphicon-remove').show();
+
+    stateSubmitButton(submit);
+
+  }
+
+  function setStandard(element){
+
+    element.closest('div').removeClass().addClass('input-group has-feedback');
+
+    element.next().removeClass().addClass('glyphicon form-control-feedback').hide();
+
+  }
+
+
+  function showError(element){
+
+    element.closest('.form-group').next().slideDown();
+
+  }
+
+  function hideError(element){
+
+    element.closest('.form-group').next().slideUp();
+
+  }
+
+  function notNull(element){
+
+    var elementLength = element.val().length;
+
+    var result = false;
+
+    if(elementLength != 0){
+
+      result = true;
+
+    }
+
+    return result;
+
+  }
+
+  function treatField(element){
+
+    if(notNull(element)){
+
+      setOk(element);
+
+      hideError(element);
+
+    } else {
+
+      setKo(element);
+
+      showError(element);
+
+    }
+
+  }
+
+  function treatEmailField(element){
+
+    var email   = element.val();
+
+    var regexp  = /([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/;
+
+    var result  = regexp.test(email);
+
+    if(result){
+
+      setOk(element);
+
+      hideError(element);
+
+    } else {
+
+      setKo(element);
+
+      showError(element);
+
+    }
+
+  }
+
+  function treatMdp1Field(element){
+
+    if(element.val().length > 5){
+
+      setOk(element);
+
+      hideError(element);
+
+    } else {
+
+      setKo(element);
+
+      showError(element);
+
+    }
+
+  }
+
+  function treatMdp2Field(element){
+
+    if(element.val() == password1.val() && element.val().length > 5){
+
+      setOk(element);
+
+      hideError(element);
+
+    } else {
+
+      setKo(element);
+
+      showError(element);
+
+    }
+
+  }
+
+
+  function disable(button){
+
+    button.prop("disabled", true);
+
+  }
+
+  function able(button){
+
+    button.prop("disabled", false);
+
+  }
+
+
+  function stateSubmitButton(){
+    if ($('#divNom').hasClass('has-success') && $('#divEmail').hasClass('has-success') && $('#divMdp1').hasClass('has-success') && $('#divMdp2').hasClass('has-success')) {
+
+        console.log("has success");
+
+        able(submit);
+
+      } else {
+
+        console.log("not success")
+
+        disable(submit);
+
+      }
+    }
+
+
+  });
--- Vues/sign-up.php
+++ Vues/sign-up.php
@@ -35,42 +35,55 @@ require_once("header.php");
 
 		<div class="form-group">
 			<label for="nom">Nom</label>
-			<div class="input-group">
+			<div id ="divNom" class="input-group">
 			  <div class="input-group-addon">@</div>
-			  <input name="nom" class="form-control" type="text" placeholder="SAM">
+			  <input id="nom" name="nom" class="form-control" type="text" placeholder="SAM">
+        <span class="glyphicon form-control-feedback" hidden="true" aria-hidden="true"></span>
 			</div>
 		</div>
+
+    <div class="alert alert-danger text-center" role="alert" hidden="true">Indiquer votre nom</div>
 
 		<div class="form-group">
 			<label for="mail">Adresse email</label>
-			<div class="input-group">
+			<div id="divEmail" class="input-group">
 		 		<div class="input-group-addon">@</div>
-		  		<input name="email" class="form-control" type="email" placeholder="sam@hotmail.com">
+		  		<input id="email" name="email" class="form-control" type="email" placeholder="sam@hotmail.com">
+          <span class="glyphicon form-control-feedback" hidden="true" aria-hidden="true"></span>
 			</div>
 		</div>
 
+    <div class="alert alert-danger text-center" role="alert" hidden="true">Email non valide</div>
+
 		<div class="form-group">
 			<label for="password">Mot de passe</label>
-			<div class="input-group">
+			<div id="divMdp1"class="input-group">
 		  		<div class="input-group-addon">
 		  			<span class="glyphicon glyphicon-lock"></span>
 		  		</div>
 		  		<input name="password" type="password" class="form-control" id="password" placeholder="*********">
+          <span class="glyphicon form-control-feedback" hidden="true" aria-hidden="true"></span>
 			</div>
 		</div>
 
+    <div class="alert alert-danger text-center" role="alert" hidden="true">MDP non valide ( min 5 charactères )</div>
+
 		<div class="form-group">
 			<label for="password">Confirmez le mot de passe</label>
-			<div class="input-group">
+			<div id="divMdp2"class="input-group">
 		  		<div class="input-group-addon">
 		  			<span class="glyphicon glyphicon-lock"></span>
 		 		 </div>
 		  		<input name="password_confirme" type="password" class="form-control" id="password" placeholder="*********">
+          <span class="glyphicon form-control-feedback" hidden="true" aria-hidden="true"></span>
 			</div>
 		</div>
 
+    <div class="alert alert-danger text-center" role="alert" hidden="true">Les MDP doivent être les mêmes</div>
+
+
 	  <div class="form-group col-lg-6 col-lg-offset-3">
-	  	 <button type="submit" class="btn btn-primary btn-block">Valider</button>
+	  	 <button name="submit" type="submit" class="btn btn-primary btn-block">Valider</button>
 	  </div>
 
 	</form>
