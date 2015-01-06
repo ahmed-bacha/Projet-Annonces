bfdd4527cc3d71bb1aff6ae43ecf833be879baa3
nbouherrou <nacerbouherrou@gmail.com>
2015-01-06 01:22:26 +0100

    SQL table AdminM

--- BDD/annonce.sql
+++ BDD/annonce.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Mer 10 Décembre 2014 à 14:41
+-- Généré le :  Mar 06 Janvier 2015 à 01:08
 -- Version du serveur :  5.5.38
 -- Version de PHP :  5.5.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `annonce`
@@ -23,11 +17,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `adminM`
+--
+
+CREATE TABLE `adminM` (
+  `id` int(10) NOT NULL,
+  `login` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mdp` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `adminM`
+--
+
+INSERT INTO `adminM` (`id`, `login`, `mdp`) VALUES
+(2, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `annonceM`
 --
 
 CREATE TABLE `annonceM` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `idUtilisateur` int(11) NOT NULL,
   `statut` tinyint(1) NOT NULL,
   `nom` varchar(255) NOT NULL,
@@ -37,14 +50,15 @@ CREATE TABLE `annonceM` (
   `prix` int(11) NOT NULL,
   `description` text NOT NULL,
   `images` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `annonceM`
 --
 
 INSERT INTO `annonceM` (`id`, `idUtilisateur`, `statut`, `nom`, `prenom`, `telephone`, `titre`, `prix`, `description`, `images`) VALUES
-(47, 29, 10, 'BOUHERROU', 'NACER', '0652685419', 'MacBook Pro', 1150, 'bon etat', '');
+(53, 29, 10, 'BOUHERROU', 'NACER', '0652685419', 'MacBook Pro000000', 1, '000000', 'image29-1003506.jpg'),
+(54, 29, 10, 'BOUHERROU', 'NACER', '0652685419', 'Site web perso', 2147483647, 'azerty', 'image29-4141287.jpg;image29-2632767.jpg');
 
 -- --------------------------------------------------------
 
@@ -53,11 +67,11 @@ INSERT INTO `annonceM` (`id`, `idUtilisateur`, `statut`, `nom`, `prenom`, `telep
 --
 
 CREATE TABLE `utilisateurM` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mdp` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(60) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `utilisateurM`
@@ -68,7 +82,7 @@ INSERT INTO `utilisateurM` (`id`, `nom`, `mdp`, `email`) VALUES
 (17, 'FrameWork', 'CakePHP', 'utilisateur_TSE_2@univ-st-etienne.fr'),
 (18, 'FrameWork', 'CakePHP', 'utilisateur_TSE_2@univ-st-etienne.fr'),
 (19, 'FrameWork', 'CakePHP', 'utilisateur_TSE_2@univ-st-etienne.fr'),
-(20, 'utilisateur_TSE_2', 'utiLISATEUR', 'utilisateur_TSE_2@univ-st-etienne.fr'),
+(20, 'FrameWork', 'CakePHP', 'utilisateur_TSE_2@univ-st-etienne.fr'),
 (21, 'utilisateur_TSE_2', 'utiLISATEUR', 'utilisateur_TSE_2@univ-st-etienne.fr'),
 (22, 'utilisateur_TSE_2', 'utiLISATEUR', 'utilisateur_TSE_2@univ-st-etienne.fr'),
 (23, 'utilisateur_TSE_2', 'utiLISATEUR', 'utilisateur_TSE_2@univ-st-etienne.fr'),
@@ -82,38 +96,47 @@ INSERT INTO `utilisateurM` (`id`, `nom`, `mdp`, `email`) VALUES
 (31, 'zadz', 'poiuyt', 'sbouherrou@yahoo.fr'),
 (32, 'utilisateur_TSE_2', 'utiLISATEUR', 'utilisateur_TSE_2@univ-st-etienne.fr'),
 (33, 'poi', 'nass25', 'samu@glu.fr'),
-(34, 'd;s,fn', 'nass25', 'gfjdhgfjd@kfjd.df');
+(34, 'd;s,fn', 'nass25', 'gfjdhgfjd@kfjd.df'),
+(35, 'utilisateur_TSE_2', 'utiLISATEUR', 'utilisateur_TSE_2@univ-st-etienne.fr');
 
 --
 -- Index pour les tables exportées
 --
 
 --
+-- Index pour la table `adminM`
+--
+ALTER TABLE `adminM`
+ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `annonceM`
 --
 ALTER TABLE `annonceM`
- ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `utilisateurM`
 --
 ALTER TABLE `utilisateurM`
- ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
+-- AUTO_INCREMENT pour la table `adminM`
+--
+ALTER TABLE `adminM`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT pour la table `annonceM`
 --
 ALTER TABLE `annonceM`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=48;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=55;
 --
 -- AUTO_INCREMENT pour la table `utilisateurM`
 --
 ALTER TABLE `utilisateurM`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
