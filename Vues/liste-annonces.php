24dba3d4c7012246a44955024240cf6a58adaedd
olivier marin <oliviermarin92@gmail.com>
2015-01-08 16:29:09 +0100

    pagination suite et fin 2
    pagination suite et fin 2

--- Vues/liste-annonces.php
+++ Vues/liste-annonces.php
@@ -71,60 +71,56 @@
 		// function to activate a fontion
 		//-------------------------------------
 
-		$(document).ready(function() {
+		$('#bloc-liste-annonces').empty();
 
-			$('#bloc-liste-annonces').empty();
+		$.get( "pagination-treatment.php", function( data ) {
 
-			$.get( "pagination-treatment.php", function( data ) {
+			var t_data = $.parseJSON(data);
 
-				var t_data = $.parseJSON(data);
+			var numeroPage = t_data[0];
 
-				var numeroPage = t_data[0];
+			var t_AnnonceData = t_data[1];
 
-				var t_AnnonceData = t_data[1];
+			for(var i in t_AnnonceData){
 
-				for(var i in t_AnnonceData){
+				var annonce = t_AnnonceData[i];
 
-					var annonce = t_AnnonceData[i];
-
-					$('#bloc-liste-annonces').append('<a href="detail-annonce.php?idAnnonce='+annonce['id']+'" >'+
-																						'<div class="col-lg-4 ">'+
-																							'<div class="panel annonce-panel">'+
-																								'<div class="panel-heading annonce-heading">'+
-																									'<span class="textCustom">'+
-																										annonce['titre']+
-																									'</span>'+
-																								'</div>'+
-																								'<div class="panel-body text-center annonce">'+
-																								'<div class="announceBody">'+
-																									'<img class="img-thumbnail img-responsive"  src="'+annonce['images']+'" alt="image">'+
-																								'</div>'+
-																								'<hr>'+
-																								'<button type="submit" name = "annonce" class="btn panel-button btn-primary btn-lg text-left" >'+
-																									annonce['prix']+" €"+
-																								'</button >'+
+				$('#bloc-liste-annonces').append('<a href="detail-annonce.php?idAnnonce='+annonce['id']+'" >'+
+																					'<div class="col-lg-4 ">'+
+																						'<div class="panel annonce-panel">'+
+																							'<div class="panel-heading annonce-heading">'+
+																								'<span class="textCustom">'+
+																									annonce['titre']+
+																								'</span>'+
 																							'</div>'+
+																							'<div class="panel-body text-center annonce">'+
+																							'<div class="announceBody">'+
+																								'<img class="img-thumbnail img-responsive"  src="'+annonce['images']+'" alt="image">'+
+																							'</div>'+
+																							'<hr>'+
+																							'<button type="submit" name = "annonce" class="btn panel-button btn-primary btn-lg text-left" >'+
+																								annonce['prix']+" €"+
+																							'</button >'+
 																						'</div>'+
 																					'</div>'+
-																				'</a>');
+																				'</div>'+
+																			'</a>');
 
-				}
+			}
 
-				$('.pagination li:first-child').hide();
+			$('.pagination li:first-child').hide();
 
-				var nbPage = t_data[2];
+			var nbPage = t_data[2];
 
-				if (numeroPage == nbPage) {
+			if (numeroPage == nbPage) {
 
-					$('.pagination li:last-child').hide();
+				$('.pagination li:last-child').hide();
 
-				} else {
+			} else {
 
-					$('.pagination li:last-child').show();
+				$('.pagination li:last-child').show();
 
-				}
-
-			});
+			}
 
 		});
 
